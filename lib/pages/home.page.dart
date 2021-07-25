91513b172381fb8f0cd38cfdadd0403dc127fd63
KartikJabreba <k.jabreba@gmail.com>
2021-07-25 19:40:02 +0530

    bottom overflow done

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -47,100 +47,103 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(left: 15.0),
-              child: Text(
-                "Hello, " + username,
-                style: TextStyle(
-                  fontSize: 38,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.black,
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 15.0),
+                child: Text(
+                  "Hello, " + username,
+                  style: TextStyle(
+                    fontSize: 38,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.black,
+                  ),
                 ),
               ),
-            ),
-            SummaryCard(
-                image: AssetImage('assets/icons/Battery.png'),
-                value: "",
-                unit: "",
-                title: batteryValue.toString() + "%",
-                color: Constants.transparent),
-          ],
-        ),
-        Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SummaryCard(
-                      image: AssetImage('assets/icons/steps.png'),
-                      title: "Footsteps",
-                      value: this.footsteps.toString(),
-                      unit: "steps",
-                      color: Constants.transparent),
-                  SizedBox(width: 10),
-                  SummaryCard(
-                      image: AssetImage('assets/icons/sleep.png'),
-                      title: "Sleep",
-                      value: this.sleep.toString(),
-                      unit: "hours",
-                      color: Constants.transparent),
-                ],
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SummaryCard(
-                      image: AssetImage('assets/icons/heartbeat.png'),
-                      title: "Heart Rate",
-                      value: this.heartrate.toString(),
-                      unit: "bpm",
-                      color: Constants.lightBlue),
-                  SizedBox(width: 10),
-                  SummaryCard(
-                      image: AssetImage('assets/icons/oxygen_.png'),
-                      title: "SPo2",
-                      value: this.spo2.toString(),
-                      unit: "%",
-                      color: Constants.lightBlue),
-                ],
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SummaryCard(
-                      image: AssetImage('assets/icons/temperature.png'),
-                      title: "Temperature",
-                      value: this.temperature.toString(),
-                      unit: "˚C",
-                      color: Constants.lightBlue),
-                  SizedBox(width: 10),
-                  SummaryCard(
-                      image: AssetImage('assets/icons/ECG.png'),
-                      title: "ECG",
-                      value: "??",
-                      unit: "",
-                      color: Constants.lightBlue),
-                ],
-              ),
+              SummaryCard(
+                  image: AssetImage('assets/icons/Battery.png'),
+                  value: "",
+                  unit: "",
+                  title: batteryValue.toString() + "%",
+                  color: Constants.transparent),
             ],
           ),
-        ),
-      ],
+          Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  //crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SummaryCard(
+                        image: AssetImage('assets/icons/steps.png'),
+                        title: "Footsteps",
+                        value: this.footsteps.toString(),
+                        unit: "steps",
+                        color: Constants.transparent),
+                    SizedBox(width: 10),
+                    SummaryCard(
+                        image: AssetImage('assets/icons/sleep.png'),
+                        title: "Sleep",
+                        value: this.sleep.toString(),
+                        unit: "hours",
+                        color: Constants.transparent),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SummaryCard(
+                        image: AssetImage('assets/icons/heartbeat.png'),
+                        title: "Heart Rate",
+                        value: this.heartrate.toString(),
+                        unit: "bpm",
+                        color: Constants.lightBlue),
+                    SizedBox(width: 10),
+                    SummaryCard(
+                        image: AssetImage('assets/icons/oxygen_.png'),
+                        title: "SPo2",
+                        value: this.spo2.toString(),
+                        unit: "%",
+                        color: Constants.lightBlue),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SummaryCard(
+                        image: AssetImage('assets/icons/temperature.png'),
+                        title: "Temperature",
+                        value: this.temperature.toString(),
+                        unit: "˚C",
+                        color: Constants.lightBlue),
+                    SizedBox(width: 10),
+                    SummaryCard(
+                        image: AssetImage('assets/icons/ECG.png'),
+                        title: "ECG",
+                        value: "??",
+                        unit: "",
+                        color: Constants.lightBlue),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/profile.page.dart
+++ lib/pages/profile.page.dart
@@ -3,7 +3,6 @@ import 'package:coeus_v1/pages/deviceinfo.page.dart';
 import 'package:coeus_v1/pages/personal.profile.page.dart';
 import 'package:coeus_v1/pages/scheduling.profile.dart';
 import 'package:coeus_v1/pages/support.profile.dart';
-import 'package:coeus_v1/utils/const.dart';
 import 'package:coeus_v1/widget/button.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -19,31 +18,33 @@ class ProfilePage extends StatelessWidget {
         //           end: Alignment.bottomLeft,
         //           colors: [Constants.white, Constants.lightBlue]),
         //     ),
-        child: Column(
+        child: SingleChildScrollView(
+          child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Button(
-            title: "Personal Info",
-            nextNavigation: PersonalProfilePage(),
-            width: MediaQuery.of(context).size.width),
-        Button(
-            title: "Caregiver Details",
-            nextNavigation: SupportProfilePage(),
-            width: MediaQuery.of(context).size.width),
-        Button(
-            title: "Monitoring Schedule",
-            nextNavigation: SchedulingProfilePage(),
-            width: MediaQuery.of(context).size.width),
-        Button(
-            title: "Advanced Settings",
-            nextNavigation: AdvancedSettingsProfilePage(),
-            width: MediaQuery.of(context).size.width),
-        Button(
-            title: "Device Info",
-            nextNavigation: DeviceInfoProfilePage(),
-            width: MediaQuery.of(context).size.width),
+          Button(
+              title: "Personal Info",
+              nextNavigation: PersonalProfilePage(),
+              width: MediaQuery.of(context).size.width),
+          Button(
+              title: "Caregiver Details",
+              nextNavigation: SupportProfilePage(),
+              width: MediaQuery.of(context).size.width),
+          Button(
+              title: "Monitoring Schedule",
+              nextNavigation: SchedulingProfilePage(),
+              width: MediaQuery.of(context).size.width),
+          Button(
+              title: "Advanced Settings",
+              nextNavigation: AdvancedSettingsProfilePage(),
+              width: MediaQuery.of(context).size.width),
+          Button(
+              title: "Device Info",
+              nextNavigation: DeviceInfoProfilePage(),
+              width: MediaQuery.of(context).size.width),
       ],
-    ));
+    ),
+        ));
   }
 }
